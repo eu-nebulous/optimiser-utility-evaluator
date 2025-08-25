c214e70be7cf09fae0d8b9b8a429929182e79639
robert.sanfeliu <robert.sanfeliu@eurecat.org>
2025-08-25 13:26:15 +0200

    Add a try-catch to the general message handler to make the component more robust

--- utility-evaluator/src/main/java/eu/nebulous/utilityevaluator/communication/exnconnector/DslGenericMessageHandler.java
+++ utility-evaluator/src/main/java/eu/nebulous/utilityevaluator/communication/exnconnector/DslGenericMessageHandler.java
@@ -15,44 +15,45 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.extern.slf4j.Slf4j;
 
-
 @Slf4j
 //@Component
 public class DslGenericMessageHandler extends Handler {
 // This class handles the dsl.generic message that contains all information needed for application deployment
 // The example of this message is provided: TODO
 
+	private UtilityEvaluatorController controller;
+	private static final ObjectMapper mapper = new ObjectMapper();
 
-    private UtilityEvaluatorController controller;
-    private static final ObjectMapper mapper = new ObjectMapper();
-
-
-    public DslGenericMessageHandler(Context exnContext, Publisher performanceIndicatorPublisher){
-        super();
-        this.controller = new UtilityEvaluatorController(exnContext, performanceIndicatorPublisher);
-    }
-
-    @Override
-    public void onMessage(String key, String address, Map body, Message message, Context context) {
-        log.info("Received by custom handler {} => {} = {}", key,address,String.valueOf(body));
-        log.info("Body={}", body.toString());
-
-        //ObjectMapper objectMapper = new ObjectMapper();
-        //GenericDSLMessage genericDSLMessage = objectMapper.readValue(body.toString(), GenericDSLMessage.class);
-        //Application appFromMessage = new Application(genericDSLMessage);
-
-        JsonNode appMessage = mapper.valueToTree(body);
-        Application app;
-	try {
-	    app = new Application(appMessage);
-	} catch (JsonProcessingException e) {
-            log.error("Could not read app creation message", e);
-            return;
+	public DslGenericMessageHandler(Context exnContext, Publisher performanceIndicatorPublisher) {
+		super();
+		this.controller = new UtilityEvaluatorController(exnContext, performanceIndicatorPublisher);
 	}
-        log.info("Application {}, with name {}, has variables: {}", app.getApplicationId(), app.getApplicationName(), app.getVariables().toString());
-        app = controller.createInitialCostPerformanceIndicators(app);
-    }
 
-        
-      
+	@Override
+	public void onMessage(String key, String address, Map body, Message message, Context context) {
+		try {
+			log.info("Received by custom handler {} => {} = {}", key, address, String.valueOf(body));
+			log.info("Body={}", body.toString());
+
+			// ObjectMapper objectMapper = new ObjectMapper();
+			// GenericDSLMessage genericDSLMessage = objectMapper.readValue(body.toString(),
+			// GenericDSLMessage.class);
+			// Application appFromMessage = new Application(genericDSLMessage);
+
+			JsonNode appMessage = mapper.valueToTree(body);
+			Application app;
+
+			app = new Application(appMessage);
+
+			log.info("Application {}, with name {}, has variables: {}", app.getApplicationId(),
+					app.getApplicationName(), app.getVariables().toString());
+			app = controller.createInitialCostPerformanceIndicators(app);
+		} catch (JsonProcessingException e) {
+			log.error("Could not read app creation message", e);
+			return;
+		} catch (Exception ex) {
+			log.error("DslGenericMessageHandler onMessage error", ex);
+		}
+	}
+
 }
