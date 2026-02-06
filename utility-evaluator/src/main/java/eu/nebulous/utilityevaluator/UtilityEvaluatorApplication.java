3c2ac797c0416b2ce090a360bfcca6f230884a19
robert.sanfeliu <robert.sanfeliu@eurecat.org>
2026-02-06 16:28:22 +0100

    dummy commit to force rebuild

--- utility-evaluator/src/main/java/eu/nebulous/utilityevaluator/UtilityEvaluatorApplication.java
+++ utility-evaluator/src/main/java/eu/nebulous/utilityevaluator/UtilityEvaluatorApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /* This is the main class which starts the Utility Evaluator */
 @SpringBootApplication
 public class UtilityEvaluatorApplication {
-
     
     public static void main(String[] args) {
         SpringApplication.run(UtilityEvaluatorApplication.class, args);
